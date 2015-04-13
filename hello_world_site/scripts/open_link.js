213bd3de4e585fa05fcffb63a269bfd9db2b2e03
iliyan <iliyan.germanov971@gmail.com>
2015-04-13 19:29:39 +0300

    Adding WEB DESIGN project.

---
+++ hello_world_site/scripts/open_link.js
@@ -0,0 +1,10 @@
+function OpenLink(link) {
+	Url = new Array;
+	Url[0] = "cpp.html";
+	Url[1] = "android_developing.html";
+	Url[2] = "web_design.html";
+	Url[3] = "python_django.html";
+
+	Chooselink = Math.floor(Math.random() * (Url.length + 1));
+	window.open(link,'_blank');
+}
